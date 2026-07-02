271f68f8e7002a2349895639de590610abd24199
anshiiiii1113 <anshital3@gmail.com>
2026-07-02 14:28:48 +0530

    example commit message

---
+++ Main.java
@@ -0,0 +1,16 @@
+// public class Main {
+//     public static void main(String[] args) {
+//         System.out.println("My name is Anshita");
+//     }
+// }
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+
+        System.out.println("Sum = " + sum);
+
+    }
+}
