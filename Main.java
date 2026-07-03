8b52821a4c4a87df92950ea12e0ab14eab572458
anshiiiii1113 <anshital3@gmail.com>
2026-07-03 20:34:48 +0530

    3july

--- Main.java
+++ Main.java
@@ -8,9 +8,27 @@ public class Main {
 
         int a = 10;
         int b = 20;
+        int multiplication = a * b;
         int sum = a + b;
+        int division = b / a ;
 
         System.out.println("Sum = " + sum);
+        System.out.println("Multiplication = " + multiplication);
+        System.out.println("Division = " + division);
+
+        System.out.println("a==b: " + (a == b));
+        System.out.println("a!=b: " + (a != b));
+        
+        int age = 25;
+
+        System.out.println(age > 18 && age < 60);
+        System.out.println(age < 18 || age > 60);
+        int z=25;
+        System.out.println(z+=25);
+        System.out.println(z-=25);
+        System.out.println(z*=25);
+
+
 
     }
 }
---
+++ first.java
@@ -0,0 +1,31 @@
+// import java.util.Scanner;
+
+// public class first
+// {
+
+//     public static void main(String[] args) {
+
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.print("Enter number: ");
+//         int num = sc.nextInt();
+
+//         if(num % 2 == 0)
+//         {
+//             System.out.println("Even");
+//         }
+//         else
+//         {
+//             System.out.println("Odd");
+//         }
+
+//         sc.close();
+//     }
+// // }
+// public class first{
+
+//     public static void main(String[] args){
+//      System.out.println("say");
+//         System.out.print("Hello World");
+//     }
+// }
